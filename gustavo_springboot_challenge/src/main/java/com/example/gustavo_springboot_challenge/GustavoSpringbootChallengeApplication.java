13ba0a8a6263060db100f574a83d660ec863b76f
Gustavo Oliveira <gustavogustgu@gmail.com>
2023-09-14 17:45:22 -0300

    teste branch-development

---
+++ gustavo_springboot_challenge/src/main/java/com/example/gustavo_springboot_challenge/GustavoSpringbootChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gustavo_springboot_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustavoSpringbootChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GustavoSpringbootChallengeApplication.class, args);
+	}
+
+}
---
+++ gustavo_springboot_challenge/src/test/java/com/example/gustavo_springboot_challenge/GustavoSpringbootChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gustavo_springboot_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavoSpringbootChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
